6edb693a65fa7147de451eb9b993d1dcd38a1a62
iremnuromgen <iremnuromgen@gmail.com>
2025-02-20 19:25:02 +0300

    Update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3725473361"
+    serviceWorkerVersion: "3831728748"
   }
 });
